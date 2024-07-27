59c5971549cf03edf0de157e2cc30fb33bcbaaa2
DS-Koolaid <matthew.jackoski@praetorian.com>
2024-07-26 22:01:51 -0700

    fixing tests

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// index.js
-const express = require('express');
+import express from 'express';
+
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+export { app };
